3b6b20413f42447f3571649dad50a5f16244019b
Jonathan Bondhus <jonb@myglobaldata.com>
2017-02-09 23:06:27 -0600

    Add logging

--- main.py
+++ main.py
@@ -1,48 +1,64 @@
+import logging.handlers
+import traceback
+
 from sqlalchemy import create_engine
-from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import sessionmaker
 
 from Config import Config
 from Directions import Directions
 from Models import *
 
-Base = automap_base()
+my_logger = logging.getLogger('traffic-logger')
+my_logger.setLevel(logging.DEBUG)
 
-config = Config().get()
+handler = logging.FileHandler('logs/traffic-logger.log')
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-engine = create_engine(config['sql_connection_string'])
-Session = sessionmaker(bind=engine)
+handler.setFormatter(formatter)
 
-session = Session()
+my_logger.addHandler(handler)
 
-api_key = config['google_api_key']
+try:
+    config = Config().get()
 
-forward_route_id = 1
-backward_route_id = 2
+    engine = create_engine(config['sql_connection_string'])
+    Session = sessionmaker(bind=engine)
 
-forward_directions = Directions(session, api_key, forward_route_id)
-backward_directions = Directions(session, api_key, backward_route_id)
+    session = Session()
 
-print('Forward: ' + str(forward_directions.get_traffic_seconds()))
-print('Backward: ' + str(backward_directions.get_traffic_seconds()))
+    api_key = config['google_api_key']
 
-print('Inserting data...')
+    forward_route_id = 1
+    backward_route_id = 2
 
-session.add_all(
-    [
-        Transit(
-            route_id=forward_route_id,
-            duration=forward_directions.get_seconds(),
-            all_data=forward_directions.get_all_data()
-        ),
-        Transit(
-            route_id=backward_route_id,
-            duration=backward_directions.get_seconds(),
-            all_data=backward_directions.get_all_data()
-        )
-    ]
-)
+    forward_directions = Directions(session, api_key, forward_route_id)
+    backward_directions = Directions(session, api_key, backward_route_id)
 
-session.commit()
+    my_logger.addHandler(handler)
 
-print('Inserted data')
+    my_logger.info('Forward Seconds: ' + str(forward_directions.get_seconds()))
+    my_logger.info('Backward Seconds: ' + str(backward_directions.get_seconds()))
+
+    my_logger.debug('Inserting data')
+
+    session.add_all(
+        [
+            Transit(
+                route_id=forward_route_id,
+                duration=forward_directions.get_seconds(),
+                all_data=forward_directions.get_all_data()
+            ),
+            Transit(
+                route_id=backward_route_id,
+                duration=backward_directions.get_seconds(),
+                all_data=backward_directions.get_all_data()
+            )
+        ]
+    )
+
+    session.commit()
+
+    my_logger.debug('Inserted data')
+except Exception as e:
+    my_logger.error(str(e))
+    my_logger.info(traceback.format_exc())
